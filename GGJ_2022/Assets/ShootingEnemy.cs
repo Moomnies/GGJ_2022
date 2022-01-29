c7a438151aa45333f341deb4527af64ce078c9ed
Moomnies <vera.dawn94@gmail.com>
2022-01-29 19:05:58 +0100

    Import

---
+++ GGJ_2022/Assets/ShootingEnemy.cs
@@ -0,0 +1,58 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Pathfinding;
+
+public class ShootingEnemy : MonoBehaviour
+{
+    [Header("Stuff you can Fuck with")]
+    [SerializeField]
+    float cooldownTimer;
+
+    [Header("Jonas & Vera Stuff")]
+    [SerializeField]
+    AIPath aiPath;
+
+    Vector3 playerPosition;    
+
+    Cooldown seePlayerShootCooldown;
+    Cooldown shootCooldown;
+
+    private void Start() {
+
+        seePlayerShootCooldown = new Cooldown(cooldownTimer, true);
+        shootCooldown = new Cooldown(cooldownTimer, false);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision) {
+        
+        if(collision.gameObject.GetComponent<PlayerScript>() != null) {
+
+            aiPath.canMove = false;
+            shootCooldown.ToggleTimer();
+        }
+    }
+
+    private void OnTriggerStay2D(Collider2D collision) {
+
+        if (collision.gameObject.GetComponent<PlayerScript>() != null) {
+
+            shootCooldown.Tick();
+
+            if (shootCooldown.CurrentValue <= 0) {
+
+                Debug.Log("Shoot");
+                shootCooldown.ResetTimer();
+
+            }
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision) {
+
+        if (collision.gameObject.GetComponent<PlayerScript>() != null) {
+
+            aiPath.canMove = true;
+        }
+    }
+}
