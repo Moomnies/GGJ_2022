0ad0aa77c7c1b8cf461b39f0a7e2fa467d415174
InDeWalvis <jonasbjansen@gmail.com>
2022-01-29 22:48:17 +0100

    Imported Photon package, added first version menu scene's and scripts

---
+++ GGJ_2022/Assets/_Scripts/Multiplayer/Menu's/StaticVars.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class StaticVars
+{
+    public static string roomName;
+    public static int playerType;
+}
